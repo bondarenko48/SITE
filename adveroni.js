d8c7be58788a1a00ebeb3992c3d06442b55248ba
bondarenko48 <bondarenko48@gmail.com>
2026-06-30 15:30:51 +0200

    change hello@adveroni.pl

--- adveroni.js
+++ adveroni.js
@@ -581,7 +581,7 @@
   const emailCopyBtn = $('#emailCopyBtn');
   if (emailCopyBtn) {
     emailCopyBtn.addEventListener('click', () => {
-      navigator.clipboard.writeText('hola@adveroni.pl').then(() => {
+      navigator.clipboard.writeText('hello@adveroni.pl').then(() => {
         emailCopyBtn.textContent = 'E-mail skopiowany';
         emailCopyBtn.classList.add('copied');
         setTimeout(() => {
